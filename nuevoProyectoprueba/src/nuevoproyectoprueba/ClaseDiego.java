17234718e6340c14655522d85861c4ab6d4b0041
diakza <diegodearmas01@gmail.com>
2023-08-09 11:39:59 -0300

    creacion de clase
    cree un menu y pase las carpetas

---
+++ nuevoProyectoprueba/src/nuevoproyectoprueba/ClaseDiego.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package nuevoproyectoprueba;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author diakz
+ */
+public class ClaseDiego {
+    public void menu() {
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Bienvenido al menú del repositorio ");
+        System.out.println("1. puto el que lee ");
+        System.out.println("2. que es lo que haces culeado? ");
+        System.out.println("3. ");
+        System.out.println("4. ");
+        System.out.println("5. Salir");
+        
+        System.out.print("Ingrese la opción que desea: ");
+        int opcion = leer.nextInt();
+        
+        switch (opcion) {
+            case 1: System.out.println("weiyurgiouwer");
+                
+                break;
+            case 2:
+                
+                break;
+            case 3:
+                
+                break;
+            case 4:
+                
+                break;
+            case 5:
+                
+                break;
+           
+                
+        }
+}
