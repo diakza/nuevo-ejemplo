f6c9cb5eb17fb470d66825fa26ecec61f1ce2d18
diakza <diegodearmas01@gmail.com>
2023-08-09 12:02:11 -0300

    correccion de codigo
    me paso de vergaaaa

--- nuevoProyectoprueba/src/nuevoproyectoprueba/ClaseDiego.java
+++ nuevoProyectoprueba/src/nuevoproyectoprueba/ClaseDiego.java
@@ -11,6 +11,7 @@ import java.util.Scanner;
  * @author diakz
  */
 public class ClaseDiego {
+
     public void menu() {
         Scanner leer = new Scanner(System.in);
         System.out.println("Bienvenido al menú del repositorio ");
@@ -19,27 +20,29 @@ public class ClaseDiego {
         System.out.println("3. ");
         System.out.println("4. ");
         System.out.println("5. Salir");
-        
+
         System.out.print("Ingrese la opción que desea: ");
         int opcion = leer.nextInt();
-        
+
         switch (opcion) {
-            case 1: System.out.println("weiyurgiouwer");
-                
+            case 1:
+                System.out.println("weiyurgiouwer");
+
                 break;
             case 2:
-                
+                System.out.println(" son unos genios");
+
                 break;
             case 3:
-                
+
                 break;
             case 4:
-                
+
                 break;
             case 5:
-                
+
                 break;
-           
-                
+
         }
+    }
 }
